90508741072bce46b5af651340daaafbce8952b9
Jimin Huh <jiminhuh95@gmail.com>
2020-10-19 17:41:43 -0500

    Initial Commit

---
+++ MemorizeSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemorizeSwift
+//
+//  Created by Jimin Huh on 10/19/20.
+//  Copyright © 2020 Jimin Huh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
