e7f10290b381a92dab83eae437ad23c1e892c721
Jimin Huh <jiminhuh95@gmail.com>
2020-10-21 09:38:39 -0500

    adding reactiveness with observableObject & ObservedObject. Also usage of @Published. Usage of mutaging on a func that will need changing.

--- MemorizeSwift/EmojiMemoryGameView.swift
+++ MemorizeSwift/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  MemorizeSwift
 //
 //  Created by Jimin Huh on 10/19/20.
@@ -8,8 +8,8 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var viewModel: EmojiMemoryGame 
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame 
     var body: some View {
         HStack {
             ForEach(viewModel.cards) { card in
@@ -48,6 +48,6 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: EmojiMemoryGame())
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
     }
 }
